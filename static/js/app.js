962f29fffdecaecb11ced8b3a1c87b6de0577abf
bripapp <brianna.papparotto@gmail.com>
2021-04-26 03:08:42 -0400

    troubleshoot loading errors

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,10 @@
-// from data.js
-var tableData = data;
-console.log(tableData);
+// // from data.js
+// var tableData = data;
+// console.log(tableData);
 
-// YOUR CODE HERE!
-// reference table body
-var tbody = d3.select('tbody');
+// // YOUR CODE HERE!
+// // reference table body
+// var tbody = d3.select('tbody');
 
-// print table data to console
-console.log(data);
+// // print table data to console
+// console.log(data);
