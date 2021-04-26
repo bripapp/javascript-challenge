f9a17aea97e37c8c13a148334af9df8e7f069f17
bripapp <brianna.papparotto@gmail.com>
2021-04-26 02:56:58 -0400

    reference table body

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,10 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+// reference table body
+var tbody = d3.select('tbody');
+
+// print table data to console
+console.log(data);
