cdcceeeee5f0c2369ca686fa23fcff27cbe68c9c
bripapp <brianna.papparotto@gmail.com>
2021-04-29 16:53:47 -0400

    clean app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,34 @@
-// Get references to the tbody element, input fields and button
-var $tbody = document.querySelector("tbody");
-var $dateInput = document.querySelector("#datetime");
-var $searchBtn = document.querySelector("#search");
-var $resetBtn = document.querySelector("#reset");
+// reference tbody, user input, search button, reset button
+let $tbody = document.querySelector("tbody");
+let $dateInput = document.querySelector("#datetime");
+let $searchBtn = document.querySelector("#search");
+let $resetBtn = document.querySelector("#reset");
 
-// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-$searchBtn.addEventListener("click", handleSearchButtonClick);
+// call clickSearch when clicked
+$searchBtn.addEventListener("click", clickSearch);
 
-// Add an event listener to the resetButton, call handleResetButtonClick when clicked
-$resetBtn.addEventListener("click", handleResetButtonClick);
+// call clickReset when clicked
+$resetBtn.addEventListener("click", clickReset);
 
 // Create a copy of the data
-var tableData = data;
+let tableData = data;
 
-// Build table with non-filtered data
+// function to build table with unfiltered data
 function renderTable() {
   $tbody.innerHTML = "";
+
   for (var i = 0; i < tableData.length; i++) {
-    // Get current address object and fields
+
+    // current address of object and fields
     var address = tableData[i];
     console.log(address)
     var fields = Object.keys(address);
-    // Create new row in tbody, set index to be i + startingIndex
+
+    // create new row in tbody
     var $row = $tbody.insertRow(i);
+      
+    // For each field, create new cell and fill with current address value
     for (var j = 0; j < fields.length; j++) {
-      // For each field in address object, create new cell and set inner text to be current value at current address field
       var field = fields[j];
       var $cell = $row.insertCell(j);
       $cell.innerText = address[field];
@@ -32,11 +36,10 @@ function renderTable() {
   }
 }
 
-// Build search table for filtered data
-function handleSearchButtonClick() {
+// filter data by date input when search button is clicked
+function clickSearch() {
   var filterDate = $dateInput.value;
-  
-  // Filter on date
+
   if (filterDate != "") {
     tableData = data.filter(function (address) {
       var addressDate = address.datetime;
@@ -44,92 +47,13 @@ function handleSearchButtonClick() {
     });
   }
   else { tableData };
-
   renderTable();
 }
 
-// Clear all the fields
-function handleResetButtonClick() {
+// clear fields
+function clickReset() {
   renderTable();
 }
 
-// Render the table for the first time on page load
+// render table on page load
 renderTable();
-// // Starter Code
-// let tableData = data;
-
-// // Creating References
-// let $tbody = d3.select("tbody");
-// let button = d3.select("#filter-btn");
-// let inputFieldDate = d3.select("#datetime");
-// let inputFieldCity = d3.select("#city");
-
-
-// let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-// // console.log(columns);
-
-
-// // Inputing the data into the HTML
-// let addData = (dataInput) => {
-//     dataInput.forEach(ufoSightings => {
-//         let row = $tbody.append("tr");
-//         columns.forEach(column => row.append("td").text(ufoSightings[column])
-//         )
-//     });
-// }
-// addData(tableData);
-
-
-// // Creating an Event Listener for the Button
-// // Setting up the Filter Button for Date and City
-// button.on("click", () => {
-//     d3.event.preventDefault();
-    
-//     let inputDate = inputDate.property("value").trim();
-//     let inputCity = inputCity.property("value").toUpperCase().trim();
-//     let inputState = inputState.property("value").toUpperCase().trim();
-//     let inputCountry = inputCountry.property("value").toUpperCase().trim();
-//     let inputShape = inputShape.property("value").toUpperCase().trim();
-    
-//     let filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-//     let filterCity = tableData.filter(tableData => tableData.city === inputCity);
-//     let filterState = tableData.filter(tableData => tableData.state === inputState);
-//     let filterCountry = tableData.filter(tableData => tableData.country === inputCountry);
-//     let filterShape = tableData.filter(tableData => tableData.shape === inputShape);
-
-//     let filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity && tableData.state === inputState && tableData.country === inputCountry && tableData.shape === inputShape);
-//     $tbody.html("");
-
-//     let response = {
-//         filterDate, filterCity, filterCombinedData
-//     }
-
-//     // let response = {
-//     //     filterDate, filterCity, filterCombinedData, filterState, filterCountry, filterShape
-//     // }
-
-//     // if(response.filterDate.length !== 0) {
-//     //     addData(filterDate);
-//     // }
-
-//     // Top if only works for filtering the date
-//     // Need to accommodate for combining multiple filters, needed to create a new var for it
-//     if(response.filterCombinedData.length !== 0) {
-//         addData(filterCombinedData);
-//     }
-
-//     // else if(response.filterCity.length !== 0){
-//     //     addData(filterCity);
-//     // }
-//         else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
-//             addData(filterDate) || addData(filterCity);
-//         }
-
-//         // else if(response.filterCombinedDateState.length === 0 && ((response.filterDate.length !== 0 || response.filterState.length !== 0))) {
-//         //     addData(filterDate) || addData(filterState);
-//         // }
-
-//         else {
-//             $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
-//         }
-// })
